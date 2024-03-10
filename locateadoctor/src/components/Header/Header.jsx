68063d1e04f3838bd894bb376e3edb26447f4222
Ravi-1701 <srkreddy.1701@gmail.com>
2024-03-10 23:23:25 +0530

    Header Added for Locate a Doctor

---
+++ locateadoctor/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="main-header">
+      <img src="assets/logo.svg" alt="logo" />
+      <div>Locate a Doctor</div>
+    </div>
+  );
+}
