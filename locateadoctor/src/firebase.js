49ea94efaa0e1cbcf9a9d2e0b36d9e3f9870c0af
saikumarreddybarda <saikumarreddybarda@gmail.com>
2024-03-11 00:07:08 +0530

    Firebase Updated

--- locateadoctor/src/firebase.js
+++ locateadoctor/src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyDLXpGXXFvPXRzsOhHxx0M8P3QTvdt3JE0",
-  authDomain: "healthcare-3ceec.firebaseapp.com",
-  databaseURL: "https://healthcare-3ceec-default-rtdb.firebaseio.com",
-  projectId: "healthcare-3ceec",
-  storageBucket: "healthcare-3ceec.appspot.com",
-  messagingSenderId: "443866244431",
-  appId: "1:443866244431:web:a1564bde30bda03c57913a",
-  measurementId: "G-M9HXYJZSGG"
+  apiKey: "AIzaSyANZfTEwP4mSsVcZ7ZOWCXhvrFR8hFPfV0",
+  authDomain: "locate-a-doctor-d02ae.firebaseapp.com",
+  projectId: "locate-a-doctor-d02ae",
+  storageBucket: "locate-a-doctor-d02ae.appspot.com",
+  messagingSenderId: "464437985962",
+  appId: "1:464437985962:web:3b4552afd92163159246d3",
+  measurementId: "G-BNNS1GGRF4"
 };
 
 const app = initializeApp(firebaseConfig);
