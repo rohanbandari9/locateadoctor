894cee3b642365ff3c96217c213915af058c7113
Ravi-1701 <srkreddy.1701@gmail.com>
2024-03-12 12:18:36 +0530

    Login Page bugs fixed

--- locateadoctor/src/Pages/Login/Login.jsx
+++ locateadoctor/src/Pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import UserAuth from './UserAuth';
 import "./Login.css";
 import Button from "../../components/ui/Button";
 import { query, where, getDocs, collection } from 'firebase/firestore';
+
 export default function Login({ setLoggedUserId }) {
   const navigate = useNavigate();
   const [errors, setErrors] = useState({});
@@ -17,36 +18,30 @@ export default function Login({ setLoggedUserId }) {
   const handleOnClick = async () => {
     const CheckData = UserAuth(values);
     setErrors(CheckData);
-    const hasNoErrors = Object.values(CheckData).every(error => error === '');
+    const hasNoErrors = isInputValid(CheckData);
+
     if (hasNoErrors) {
-      // const username = values.Username;
       try {
-        // Create a query to search for the user in both 'Users' and 'Doctors' collections
         const userQuery = query(collection(db, "Users"), where("UserName", "==", values.Username));
         const doctorQuery = query(collection(db, "Doctors"), where("UserName", "==", values.Username));
         
-        // Execute both queries concurrently
         const [userSnapshot, doctorSnapshot] = await Promise.all([
           getDocs(userQuery),
           getDocs(doctorQuery)
         ]);
         
-        // Check if the user exists in either 'Users' or 'Doctors' collection
         if (!userSnapshot.empty || !doctorSnapshot.empty) {
           const userDocs = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
           const doctorDocs = doctorSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-          
-          // Combine results from both collections
           const allDocs = [...userDocs, ...doctorDocs];
           
-          // Check password and navigate based on user type
-          const foundUser = allDocs.find(doc => doc.Password === values.Password);
+          const foundUser = findUserByPassword(allDocs, values.Password);
           if (foundUser) {
             localStorage.setItem("LogInUserId", foundUser.id);
             if (foundUser.UserType === 'Patient') {
               navigate('/home');
             } else if (foundUser.UserType === 'Admin') {
-              console.log('Logged user type is Admin');
+              navigate('/Admin');
             } else {
               navigate('/doctor');
             }
@@ -57,10 +52,33 @@ export default function Login({ setLoggedUserId }) {
           setErrors({ errorMessage: `No user found with Username ${values.Username}` });
         }
       } catch (error) {
-        console.error("Error searching for user:", error);
+        handleDatabaseError(error);
       }
     }
   };
+
+  const findUserByPassword = (users, password) => {
+    return users.find(user => comparePasswords(user.Password, password));
+  };
+
+  const comparePasswords = (storedPassword, inputPassword) => {
+    console.log(storedPassword,inputPassword);
+    if(storedPassword == inputPassword){
+      return true;
+    }
+    else{
+      return false;
+    }
+  };
+
+  const isInputValid = (errors) => {
+    return Object.values(errors).every(error => error === '');
+  };
+
+  const handleDatabaseError = (error) => {
+    console.error("Error searching for user:", error);
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center ">
@@ -72,7 +90,6 @@ export default function Login({ setLoggedUserId }) {
             <div>Username</div>
             <input type="text" name='Username' className='LoginInput' onChange={handleChange} />
             {errors.Username && <div className="error-message">{errors.Username}</div>}
-
           </div>
           <div className="d-flex flex-column g-1 px-2 mb-3 login-inputs">
             <div>Password</div>
